d3f23cc195bb3842df33263127ec34a72cc4ad35
Kurt Bellian <kelliano@gmail.com>
2019-06-16 21:04:16 +1200

    Added code for cards

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-/*
- * Create a list that holds all of your cards
- */
+//create list to hold all cards
+const allCards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bomb", "fa fa-bomb", "fa fa-bicycle", "fa fa-bicycle"]
 
+//get the deck for adding cards
+const grabDeck = document.querySelector(".deck");
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//Add the cards to the page
+for(let i = 0; i < allCards.length; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<i class="${allCards[i]}"></i>`;
+    grabDeck.appendChild(card);
+    
+    //Listen for card being clicked
+    card.addEventListener("click", function() {
+        console.log(card.innerHTML);
+    });
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,10 +32,11 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+ /*set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one) 
+ */
+ 
+/* 
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
